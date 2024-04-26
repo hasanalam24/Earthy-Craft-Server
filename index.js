a5fcd076f2199ee0927bad19a364af7380698ea5
hasanalam24 <hasanalam365@gmail.com>
2024-04-26 23:51:22 +0600

    mongbodb data send from client side

--- index.js
+++ index.js
@@ -26,7 +26,14 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const addCraftCollection = client.db('craftDB').collection('craftCat')
 
+        app.post('/addcraft', async (req, res) => {
+            const addNewCat = req.body
+            console.log(addNewCat)
+            const result = await addCraftCollection.insertOne(addNewCat)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
