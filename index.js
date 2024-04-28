52f77326748518f27b7322363f950d2cdffa07bc
hasanalam24 <hasanalam365@gmail.com>
2024-04-28 08:44:40 +0600

    myCart data get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -11,7 +11,7 @@ app.use(cors())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvnsypp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+// console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -40,6 +40,30 @@ async function run() {
             const result = await addCraftCollection.insertOne(addNewCat)
             res.send(result)
         })
+
+
+        //mycardlist get
+
+        app.get('/myCart/:email', async (req, res) => {
+
+            const result = await addCraftCollection.find({ email: req.params.email }).toArray()
+            res.send(result)
+        })
+        // app.patch('/addcraft', async (req, res) => {
+        //     const user = req.body
+
+        //     const filter = { email: user.email }
+        //     const result = await addCraftCollection.find({ filter: filter })
+        //     res.send(result)
+        // })
+        //delete
+        // app.get('/addcraft/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) }
+        //     const result = await addCraftCollection.deleteOne(query)
+        //     res.send(result)
+        // })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
